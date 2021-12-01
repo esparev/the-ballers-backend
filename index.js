21bc1c00da3281abb83bddeb6fda485e5455f9be
JoseMa Esparev <esparev@hotmail.com>
2021-12-01 14:35:12 -0600

    test: Routing with Express.js

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ app.get('/', (req, res) => {
 	res.send('Hello express server');
 });
 
+// News route
+app.get('/noticias', (req, res) => {
+	res.json({
+		news: [
+			{
+				title: 'BEISMICH liga mundial',
+				fecha: '26, octubre 2021',
+				message: 'Esta es una noticia individual',
+			},
+			{
+				title: 'BEISMICH liga mundial',
+				fecha: '26, octubre 2021',
+				message: 'Esta es una noticia individual',
+			},
+			{
+				title: 'BEISMICH liga mundial',
+				fecha: '26, octubre 2021',
+				message: 'Esta es una noticia individual',
+			},
+		],
+		message: 'Estas son varias noticias'
+	});
+});
+
+// Individual news route
+app.get('/noticias/noticia', (req, res) => {
+	res.json({
+		title: 'BEISMICH liga mundial',
+		fecha: '26, octubre 2021',
+		message: 'Esta es una noticia individual',
+	});
+});
+
 // Listen express app in port
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
