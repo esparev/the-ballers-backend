2de1ec801bef993374f6ec254a5334b122fe0f7e
JoseMa Esparev <esparev@hotmail.com>
2021-12-01 14:21:26 -0600

    create: First HTTP server with Express.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello nodemon');
+const express = require('express');
+// Create express app
+const app = express();
+const port = 3000;
+
+// Define route '/' and send a response to the client
+app.get('/', (req, res) => {
+	res.send('Hello express server');
+});
+
+// Listen express app in port
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
