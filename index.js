b4d17117edc4945eb857cc9c23e5e767542dfd25
JoseMa Esparev <esparev@hotmail.com>
2021-12-03 00:28:57 -0600

    fix: CORS problem

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
+
 const {
 	errorHandler,
 	boomErrorHandler,
 } = require('./middlewares/error.handler');
+
 // Create express app
 const app = express();
 const port = 3000;
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 routerApi(app);
+// Error middlewares
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
-	res.send('Hola');
+	res.send('BEISMICH API');
 });
 
 // Listen express app in port
