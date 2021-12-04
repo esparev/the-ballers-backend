d0619591157f36a7c956be6165ba6e75b20e43f8
JoseMa Esparev <esparev@hotmail.com>
2021-12-04 00:15:22 -0600

    refactor: Better use of environment variables

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
+const { config } = require('../config/config');
 const { Client } = require('pg');
 
+// Encoding delicate data
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+// Unique URL connection to the postgres database
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 /**
- * Connects to the provided database with it's
- * credentials
+ * Connects to the provided URL connection with it's credentials
  */
 async function getConnection() {
-	const client = new Client({
-		host: 'localhost',
-		port: 5432,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-	});
+	const client = new Client({ connectionString: URI });
 	await client.connect();
 	return client;
 }
--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+const { config } = require('../config/config');
 const { Pool } = require('pg');
 
+// Encoding delicate data
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+// Unique URL connection to the postgres database
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 /**
- * Connects to the provided database with it's
- * credentials only once to avoid unnecessary or
- * excessive request connection to the same database
+ * Connects to the database provided within the url
+ * only once to avoid unnecessary or excessive
+ * request connection to the same database
  */
-const pool = new Pool({
-	host: 'localhost',
-	port: 5432,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-});
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
