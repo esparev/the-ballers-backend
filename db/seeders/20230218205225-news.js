62cbe72ccff34ced4a1a49da8871833344dbbb06
JoseMa <esparev@hotmail.com>
2023-04-23 17:23:58 -0600

    refactor: ♻️ shorten news and tournaments descriptions

--- db/seeders/20230218205225-news.js
+++ db/seeders/20230218205225-news.js
@@ -8,12 +8,9 @@ module.exports = {
 			{
 				slug: 'the-ballers-club-makes-the-mlb',
 				title: "The ballers' club makes the mlb",
-				description:
-					`In a stunning move that has sent shockwaves throughout the sporting world, the Ballers' club has announced that they will be making the jump to Major League Baseball (MLB). The team, known for their dominance in basketball, is set to take the baseball world by storm with their unique blend of athleticism, strategy, and teamwork.
-
-					Despite the challenges that come with transitioning to a new sport, the Ballers' club is confident that they have what it takes to compete at the highest level. With a roster of talented players who excel in multiple sports, the team is well-equipped to adapt to the rigors of professional baseball and make a name for themselves on the diamond.
-					
-					For fans of the Ballers' club, this move represents an exciting new chapter in the team's storied history, as they take on a new challenge and seek to make their mark in a new sport. And for baseball fans around the world, the arrival of the Ballers' club promises to inject a new level of excitement and energy into the sport, as one of the most talented and dynamic teams in sports history takes the field.`,
+				description: `The Ballers' club has shocked the sporting world by announcing their jump to Major League Baseball (MLB). The dominant basketball team is set to take the baseball world by storm with their athleticism, strategy, and teamwork. Despite the challenges of transitioning to a new sport, the team is confident in their talented players who excel in multiple sports.
+				
+				Fans see this move as an exciting new chapter in the team's history, as they seek to make their mark in a new sport. The arrival of the Ballers' club promises to inject a new level of excitement and energy into baseball, with one of the most dynamic teams in sports history taking the field.`,
 				cover:
 					'https://www.collinsdictionary.com/images/full/baseball_557405302_1000.jpg',
 				created_at: '2022-02-18T21:08:00.190Z',
@@ -21,12 +18,9 @@ module.exports = {
 			{
 				slug: 'the-ballers-youth-won-their-first-trophy',
 				title: "The ballers' youth won their first trophy",
-				description:
-					`In a stunning upset, the Ballers' youth team emerged victorious in their first major tournament, securing their first ever trophy. The team, comprised of talented young players, battled fiercely against their seasoned opponents, showcasing an impressive level of skill and determination on the field.
-
-					Despite being considered underdogs throughout the tournament, the Ballers' youth team continued to defy expectations, defeating some of the most formidable teams in the competition with their fast-paced, high-energy style of play. Their triumph was sealed in a thrilling final match, where they emerged with a hard-fought victory, sending their fans into a frenzy of excitement and celebration.
-					
-					For many on the team, this was a moment they had been working towards for years, and the emotional scenes of joy and elation following the final whistle were a testament to their hard work and dedication. As the young players lifted the trophy high in the air, it was clear that this was just the beginning for this talented group of ballers, who had announced themselves on the world stage in the most spectacular fashion possible.`,
+				description: `The Ballers' youth team pulled off a stunning upset in their first major tournament, securing their first ever trophy against seasoned opponents. Despite being considered underdogs, they battled fiercely and showcased an impressive level of skill and determination. The fast-paced, high-energy style of play helped them defeat formidable teams, sealing their triumph in a thrilling final match.
+				
+				Emotional scenes of joy and elation followed the final whistle, as the young players lifted the trophy high in the air. This was a moment they had been working towards for years, and it marked the beginning of their journey as a talented group of ballers.`,
 				cover:
 					'https://securea.mlb.com/assets/images/5/5/2/140916552/cuts/LITTLE_LEAGUE_pdr9fsys_678kkha4.jpg',
 				created_at: '2022-08-18T21:08:00.190Z',
@@ -34,12 +28,9 @@ module.exports = {
 			{
 				slug: 'the-ballers-club-does-it-again',
 				title: "The ballers' club does it again",
-				description:
-					`In a stunning display of skill and teamwork, the Ballers' club has once again secured a major victory in their latest tournament. The team, widely regarded as one of the best in the world, dominated their opponents with a commanding performance that left no doubt as to their superiority on the court.
-
-					Led by their star players, the Ballers' club unleashed a barrage of precise passes, blistering shots, and impenetrable defense, leaving their opponents scrambling to keep up. Despite facing some of the toughest competition in the tournament, the team showed no signs of slowing down, maintaining their intensity and focus throughout each match.
-					
-					For the Ballers' club, this latest triumph is just the latest in a long line of successes, as they continue to cement their reputation as one of the most dominant forces in the sport. With their sights set on even greater achievements in the future, the team is poised to continue their winning ways, and cement their place in the annals of sporting history as one of the greatest teams of all time.`,
+				description: `The Ballers' club has once again proven their dominance in the latest tournament, securing a major victory with a stunning display of skill and teamwork. Led by their star players, they unleashed precise passes, blistering shots, and impenetrable defense, leaving their opponents scrambling.
+				
+				The team maintained their intensity and focus throughout each match, despite facing tough competition. This latest triumph adds to the Ballers' club's long line of successes, cementing their reputation as one of the most dominant teams in the world. They're poised to achieve even greater things in the future and cement their place in sporting history as one of the greatest teams ever.`,
 				cover:
 					'https://globalsportmatters.com/wp-content/uploads/2021/10/llws.jpeg',
 				created_at: '2023-02-18T21:08:00.190Z',
--- db/seeders/20230218205233-tournaments.js
+++ db/seeders/20230218205233-tournaments.js
@@ -8,14 +8,10 @@ module.exports = {
 			{
 				slug: 'ballers-youth-club-won-the-2022-tournament',
 				title: "Ballers' youth club won the 2022 tournament",
-				description:
-					`In a thrilling display of skill and determination, the Ballers' youth club emerged victorious in the highly anticipated 2022 tournament. The team, comprised of some of the most talented young athletes in the sport, battled fiercely against a field of tough opponents, showcasing their impressive abilities and tenacity in every match.
+				description: `The Ballers' youth club emerged victorious in the highly anticipated 2022 tournament, displaying skill and determination. The talented young athletes battled fiercely against tough opponents and showcased their impressive abilities and tenacity in every match. With a fast-paced, high-energy style of play, the team dominated the competition from the opening rounds, leaving no doubt as to their dominance on the court.
 
-					From the opening rounds of the tournament, it was clear that the Ballers' youth club was a team to be reckoned with. With a fast-paced, high-energy style of play that left their opponents reeling, the team dominated the competition, racking up win after win and leaving no doubt as to their dominance on the court.
-					
-					As the tournament progressed, the Ballers' youth club faced increasingly tough challenges, but they rose to the occasion with skill and grace, never losing sight of their ultimate goal. And in the final match of the tournament, they proved once and for all that they were the team to beat, defeating their opponents with a commanding performance that left no doubt as to their superiority.
-					
-					For the young players on the Ballers' youth club, this victory represents the culmination of years of hard work and dedication, and a testament to their talent and perseverance. And for fans of the sport, it is a thrilling reminder of the excitement and drama that can unfold on the court, as the best and brightest young athletes compete for glory and the chance to make their mark on the world stage.`,
+					As the tournament progressed, the Ballers' youth club faced increasingly tough challenges, but they rose to the occasion with skill and grace, never losing sight of their ultimate goal. In the final match, they proved once and for all that they were the team to beat, defeating their opponents with a commanding performance that left no doubt as to their superiority.
+					`,
 				cover:
 					'https://www.shreveportdixiebaseball.com/portals/52137/contentrotator/contentrotator637601268013542098.png',
 				created_at: '2022-08-18T21:08:00.190Z',
@@ -23,14 +19,11 @@ module.exports = {
 			{
 				slug: 'ballers-tournament-2023',
 				title: "Ballers' tournament 2023",
-				description:
-					`The highly anticipated Ballers' Tournament 2023 is set to showcase some of the most talented and dynamic basketball teams in the world, as they compete for the chance to be crowned champions and cement their place in the annals of sporting history.
+				description: `The highly anticipated Ballers' Tournament 2023 is set to showcase some of the most talented and dynamic basketball teams in the world, as they compete for the chance to be crowned champions and cement their place in the annals of sporting history.
 
 					With a roster of top-tier teams from across the globe, the Ballers' Tournament promises to be an unforgettable event, featuring thrilling games, stunning displays of athleticism, and a level of competition that is second to none. From established powerhouses to up-and-coming challengers, the tournament is sure to feature a wide range of styles and strategies, as teams battle it out for supremacy on the court.
 					
-					For fans of the sport, the Ballers' Tournament represents a chance to witness the very best in basketball, and to experience the excitement and energy of a world-class sporting event. And for the players themselves, it is an opportunity to prove themselves against the toughest competition in the world, and to demonstrate their skills and abilities on a global stage.
-					
-					As the tournament approaches, anticipation is building, and fans and players alike are gearing up for what promises to be an unforgettable display of basketball talent and passion. So mark your calendars, grab your tickets, and get ready for the Ballers' Tournament 2023 – it's going to be a tournament for the ages!`,
+					As the tournament approaches, anticipation is building, and fans and players alike are gearing up for what promises to be an unforgettable display of basketball talent and passion. So mark your calendars, grab your tickets, and get ready for the Ballers' Tournament 2023 - it's going to be a tournament for the ages!`,
 				cover:
 					'https://www.shreveportdixiebaseball.com/portals/52137/contentrotator/contentrotator637438250744259563.png',
 				created_at: '2023-02-18T21:08:00.190Z',
@@ -38,13 +31,8 @@ module.exports = {
 			{
 				slug: 'the-ballers-youth-club-is-ready-for-the-2023-tournament',
 				title: "The ballers' youth club is ready for the 2023 tournament",
-				description:
-					`As the highly anticipated Ballers' Tournament 2023 draws near, the Ballers' youth club is gearing up for what promises to be an unforgettable display of basketball talent and passion. With a roster of some of the most talented young athletes in the sport, the team is poised to take on the competition and show the world what they're made of.
+				description: `As the highly anticipated Ballers' Tournament 2023 draws near, the Ballers' youth club is gearing up for what promises to be an unforgettable display of basketball talent and passion. With a roster of some of the most talented young athletes in the sport, the team is poised to take on the competition and show the world what they're made of.
 
-					From the outset, the Ballers' youth club has been a force to be reckoned with, displaying a level of skill, athleticism, and teamwork that belies their age. And as they prepare for the tournament, they are more determined than ever to prove themselves against the toughest competition in the world, and to show that they have what it takes to be champions.
-					
-					Led by a talented and experienced coaching staff, the Ballers' youth club is honing their skills, refining their strategies, and fine-tuning every aspect of their game in preparation for the tournament. And with a deep bench of talented players who excel in all areas of the sport, the team is well-positioned to adapt to any challenge that comes their way.
-					
 					For fans of the sport, the Ballers' youth club represents the future of basketball, and a testament to the power of hard work, dedication, and a love of the game. And as they take the court at the Ballers' Tournament 2023, they will be carrying the hopes and dreams of fans around the world, and showing that the future of basketball is in good hands.`,
 				cover:
 					'https://flipgive.imgix.net/images/stories/photos/000/000/005/original/baseball-A.png?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=824&h=500&fit=crop',
